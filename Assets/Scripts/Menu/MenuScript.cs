a5e879caf90491e4c9a0254d13fc6d0c87d4a8a2
Albert Mosiałek <zieimp1@gmail.com>
2019-04-07 16:31:01 +0200

    added onclick handles in MainMenu

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject settingsButton;
+    public void Change()
+    {
+        
+        settingsButton.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
