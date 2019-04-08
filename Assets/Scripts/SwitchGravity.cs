84af39bd52599b79f0492c7a7c097d480d8575f3
twiewiora <twiewior@student.agh.edu.pl>
2019-04-08 18:50:05 +0200

    add gravity script and level 1

--- Assets/Scripts/SwitchGravity.cs
+++ Assets/Scripts/SwitchGravity.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SwitchGravity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float gravityFactor = 9.8f;
+
     void Start()
     {
-        
+        Physics2D.gravity = new Vector2(0, -gravityFactor);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Physics2D.gravity = new Vector2(0, -gravityFactor);
+            transform.eulerAngles = Vector3.zero;
+        } else if (Input.GetKeyDown(KeyCode.I))
+        {
+            Physics2D.gravity = new Vector2(0, gravityFactor);
+            transform.eulerAngles = new Vector3(0, 0, 180f);
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            Physics2D.gravity = new Vector2(-gravityFactor, 0);
+            transform.eulerAngles = new Vector3(0, 0, 270f);
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Physics2D.gravity = new Vector2(gravityFactor, 0);
+            transform.eulerAngles = new Vector3(0, 0, 90f);
+        }
     }
 }
