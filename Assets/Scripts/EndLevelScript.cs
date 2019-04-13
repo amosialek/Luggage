101901cbc34ea28118e0455a4132aca7161d738c
twiewiora <twiewior@student.agh.edu.pl>
2019-04-13 14:39:05 +0200

    add end level portal

---
+++ Assets/Scripts/EndLevelScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndLevelScript : MonoBehaviour
+{
+	private int waitingTimeInSec = 3;
+
+    void OnTriggerEnter2D()
+	{
+		GameManager.GetInstance().WinLevel();
+		StartCoroutine(WaitAndGoToMenu());
+	}
+
+	IEnumerator WaitAndGoToMenu()
+    {
+        yield return new WaitForSeconds(waitingTimeInSec);
+		SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager instance = null;
+	public GameObject winText;
+
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		} else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public static GameManager GetInstance()
+	{
+		return instance;
+	}
+
+	public void WinLevel()
+	{
+		winText.SetActive(true);
+	}
+}
