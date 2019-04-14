0da5d910a98ba37a6e72fca22f8df753c659d5e1
Albert Mosiałek <albert.mosialek@gmail.com>
2019-04-14 23:00:28 +0200

    added axe man

---
+++ Assets/Scripts/AxeMan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeMan : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody2D axeManRigidBody;
+    private float visibilityRange=10e3f;
+    public float Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        axeManRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDistance = gameObject.transform.position.x - player.transform.position.x;
+        float yDistance = gameObject.transform.position.y - player.transform.position.y;
+        if (xDistance * xDistance + yDistance * yDistance < visibilityRange * visibilityRange)
+        {
+            var gravity = Physics2D.gravity;
+            var force = (Math.Abs(gravity.x) > Math.Abs(gravity.y))
+                ? new Vector2(0f, -Math.Sign(yDistance)*Force)
+                : new Vector2(-Math.Sign(xDistance)*Force, 0f);
+            axeManRigidBody.AddForce(force);
+        }
+    }
+}
