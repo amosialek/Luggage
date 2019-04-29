b0f0427b4e5d0c311c3010aaf83c7bd0faf7cb02
twiewiora <twiewior@student.agh.edu.pl>
2019-04-29 15:12:27 +0200

    add troll, change tiles level 1,2

---
+++ Assets/Scripts/TrollScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollScript : MonoBehaviour
+{
+	public Rigidbody2D rb;
+	public float maxSpeed = 2;
+	public SpriteRenderer sr;
+
+	private float currentSpeed;
+
+    void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+		currentSpeed = maxSpeed;
+    }
+
+    void FixedUpdate()
+	{
+		rb.velocity = new Vector2(currentSpeed, 0.0f);
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "PatrolFlag") {
+			if (sr.flipX == false) {
+				sr.flipX = true;
+				currentSpeed = -maxSpeed;
+				return;
+			} else {
+				sr.flipX = false;
+			}
+			currentSpeed = maxSpeed;
+		}
+	}
+}
