25719fdc8470484d5a764e805263a519de5f2149
EmilyBlood <woda33@vp.pl>
2019-05-24 17:43:42 +0200

    Eating with space

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -14,17 +14,18 @@ public class PlayerController2D : MonoBehaviour
     void FixedUpdate()
     {
 		transform.Translate(new Vector3(Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, 0.0f, 0.0f));
+        gameManager.Score(-1);
     }
     void OnCollisionEnter2D(Collision2D col)
     {
         float collsion = Vector2.Dot(transform.position - col.transform.position, Physics2D.gravity);
 
-        if (collsion > 4)
+        if (collsion > 4 && Input.GetKeyDown("space"))
         {
             switch (col.gameObject.tag)
             {
                 case "Enemy": col.gameObject.SetActive(false); break;
-                case "Item": gameManager.Score(); col.gameObject.SetActive(false); break;
+                case "Item": gameManager.Score(100); col.gameObject.SetActive(false); break;
             }
         } else
         {
