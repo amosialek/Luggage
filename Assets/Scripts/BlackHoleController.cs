a72c656248acda98f337a1257d45d4c6d1e72c52
EmilyBlood <woda33@vp.pl>
2019-05-28 20:32:32 +0200

    Black holes and enemies collecting items

---
+++ Assets/Scripts/BlackHoleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleController : MonoBehaviour
+{
+    private Rigidbody2D portalRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.gameObject.tag)
+        {
+            case "Item": col.gameObject.SetActive(false); break;
+            case "Enemy": col.gameObject.SetActive(false); break;
+        }
+    }
+}
