e0ed53c23ada9acf58978e6463bd42ed2b6fb34e
EmilyBlood <woda33@vp.pl>
2019-05-29 13:35:11 +0200

    fix camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,17 +6,17 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 offset;
+    private Vector3 offset = new Vector3( 0, 0, -3 );
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+         transform.position = player.transform.position + offset;
     }
 }
